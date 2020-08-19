91973d205da4b9db5d7ea479024f54d71c559c28
shaghayegh9504 <69505571+shaghayegh9504@users.noreply.github.com>
2020-08-19 22:29:02 +0430

    creat book class

--- person.py
+++ person.py
@@ -6,7 +6,7 @@ class Member():
         self.address=address
         self.password = password
         self.kind=kind
-        
+
     
     def save2Db(self):
         sql = ''' INSERT INTO Member(id,name,password,kind)
@@ -16,3 +16,25 @@ class Member():
 
     def __str__(self):
         return f'Id : {self.id} , Name : {self.name} , Address : {self.address} , Password : {self.password} , Kind : {self.kind}'
+
+
+
+class Book():
+    
+    def __init__(self,id,name,edition,auther,status):
+        self.id = id
+        self.name=name
+        self.edition = edition
+        self.auther=auther
+        self.status=status
+
+
+    def save2Db(self):
+        sql = ''' INSERT INTO Book(id,name,edition,auther,status)
+        VALUES(?,?,?,?,?) '''
+        cur = conn.cursor()
+        cur.execute(sql,self.id,self.name,self.edition,self.auther,self.status)
+
+
+    def __str__(self):
+        return f'Id : {self.id} , Name : {self.name} , Edition : {self.edition} , Auther : {self.auther} , Status : {self.status}'
