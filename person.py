fb7ec9d3a366e517b2732e71ffeea9935a191111
shaghayegh9504 <69505571+shaghayegh9504@users.noreply.github.com>
2020-08-19 22:07:26 +0430

    str member

--- person.py
+++ person.py
@@ -6,10 +6,13 @@ class Member():
         self.address=address
         self.password = password
         self.kind=kind
-
+        
     
     def save2Db(self):
         sql = ''' INSERT INTO Member(id,name,password,kind)
         VALUES(?,?,?,?) '''
         cur = conn.cursor()
         cur.execute(sql,self.id,self.name,self.password,self.kind)
+
+    def __str__(self):
+        return f'Id : {self.id} , Name : {self.name} , Address : {self.address} , Password : {self.password} , Kind : {self.kind}'
