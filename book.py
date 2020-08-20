d7c6707dfed10db10ea9d6f0878049ff7ee984e9
majid <smkh1985@gmail.com>
2020-08-20 20:41:50 +0430

    Add year to book class

--- book.py
+++ book.py
@@ -1,11 +1,12 @@
 class Book():
     
-    def __init__(self,id,name,edition,auther,status):
+    def __init__(self,id,name,edition,auther,status,year):
         self.id = id
         self.name=name
         self.edition = edition
         self.auther=auther
         self.status=status
+		self.yesr = year
 
 
     def save2Db(self):
@@ -17,3 +18,4 @@ class Book():
 
     def __str__(self):
         return f'Id : {self.id} , Name : {self.name} , Edition : {self.edition} , Auther : {self.auther} , Status : {self.status}'
+
