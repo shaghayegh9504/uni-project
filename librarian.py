65ecfb832fae62271ba381576ba5b5b8d28c1d92
shaghayegh9504 <69505571+shaghayegh9504@users.noreply.github.com>
2020-08-21 12:08:57 +0430

    librarian class

---
+++ librarian.py
@@ -0,0 +1,18 @@
+class Librarian():
+    
+    def __init__(self,id,name,password):
+        self.id = id
+        self.name=name
+        self.password = password
+
+
+
+    def save2Db(self):
+        sql = ''' INSERT INTO Librarian(id,name,password)
+        VALUES(?,?,?) '''
+        cur = conn.cursor()
+        cur.execute(sql,self.id,self.name,self.password)
+
+
+    def __str__(self):
+        return f'Id : {self.id} , Name : {self.name} , Password : {self.password}'
