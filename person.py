4193aa78284b6fc69788aeba690e8e70ed3d1fa5
shaghayegh9504 <69505571+shaghayegh9504@users.noreply.github.com>
2020-08-23 00:11:15 +0430

    member

--- person.py
+++ person.py
@@ -1,16 +1,3 @@
-class Person():
-    def __init__(self,fname , lname , age , pid):
-        self.fname = fname
-		self.lname = lname
-		self.age = age
-		self.id = fpid
-		
-	def searchbook(self,book_name):
-		if book_name in ['4 tappeh' , 'adam va havva' , 'mardi be name ove' ]
-			print(book_name)
-		else :
-			print('Not found!!!')
-		
 class Member():
     
     def __init__(self,id,name,address,password,kind):
@@ -20,16 +7,15 @@ class Member():
         self.password = password
         self.kind=kind
 
-    
+
     def save2Db(self):
         sql = ''' INSERT INTO Member(id,name,password,kind)
         VALUES(?,?,?,?) '''
         cur = conn.cursor()
         cur.execute(sql,self.id,self.name,self.password,self.kind)
 
+
     def __str__(self):
         return f'Id : {self.id} , Name : {self.name} , Address : {self.address} , Password : {self.password} , Kind : {self.kind}'
 
 
-    def __repr__(self):
-        pass
